b172d2a784c5235894d840d0bdfc2043ef0d07e7
prem36999 <premdevops369@gmail.com>
2026-07-07 14:23:19 +0530

    expense shell added

--- mysql.sh
+++ mysql.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOG_FOLDER="/var/log/exoense-logs"
+LOG_FOLDER="/var/log/expense-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -33,16 +33,15 @@ echo "script started excuting at $TIMESTAMP" &>>$LOG_FILE_NAME
 
 CHECK_ROOT
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>$LOG_FILE_NAM
 VALIDATE $? "installing MYSQL-server"
 
-systemctl enable mysqld
-VALIDATE $? "enabling MYSQL server"
+systemctl enable mysqld &>>$LOG_FILE_NAM
+VALIDATE $? "enabling MYSQL"
 
-systemctl start mysqld
+systemctl start mysqld &>>$LOG_FILE_NAM
 VALIDATE $? "start server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE_NAM
 VALIDATE $? "setting root password"
-
 
