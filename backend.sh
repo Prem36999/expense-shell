f4e92d2cf2d8fef1f3f6070599f3cd47b713613e
prem36999 <premdevops369@gmail.com>
2026-07-07 20:17:40 +0530

    expense shell added

--- backend.sh
+++ backend.sh
@@ -61,8 +61,9 @@ VALIDATE $? "creat directory"
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
 VALIDATE $? " downloding backend "
 
-cd /app &>>$LOG_FILE_NAME
-VALIDATE $? "chainge poguisan"
+cd /app 
+
+rm -rf /app/*
 
 unzip /tmp/backend.zip &>>$LOG_FILE_NAME
 VALIDATE $? "unzip backend"
