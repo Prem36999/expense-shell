2a69994cdd35462a853b0c6842b3451f12b5cf8e
prem36999 <premdevops369@gmail.com>
2026-07-07 14:36:22 +0530

    expense shell added

--- mysql.sh
+++ mysql.sh
@@ -43,6 +43,6 @@ VALIDATE $? "enabling MYSQL"
 systemctl start mysqld  &>>$LOG_FILE_NAME
 VALIDATE $? "start server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE_NAM
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE_NAME
 VALIDATE $? "setting root password"
 
