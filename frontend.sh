d1d98eb303a059308633dffaf5b69e613f2964ea
prem36999 <premdevops369@gmail.com>
2026-07-10 20:47:18 +0530

    expense shell added

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOG_FOLDER="/var/log/expense-"
+LOG_FOLDER="/var/log/expense-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -33,27 +33,27 @@ echo "script started excuting at $TIMESTAMP " &>>$LOG_FILE_NAME
 
 CHECK_ROOT
 
-dnf install nginx -y 
-VALIDATE $? "installing nginx"
+dnf install nginx -y &>>$LOG_FILE_NAM
+VALIDATE $? "installing nginx" &>>$LOG_FILE_NAM
 
-systemctl enable nginx
+systemctl enable nginx &>>$LOG_FILE_NAM
 VALIDATE $? "ennableing nginx server"
 
-systemctl start nginx
+systemctl start nginx &>>$LOG_FILE_NAM
 VALIDATE $? "starting nginx server"
 
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE_NAM
 VALIDATE $? "removing old versen"
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2. &>>$LOG_FILE_NAM
 VALIDATE $? "donwloding new code"
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>$LOG_FILE_NAM
 VALIDATE $? " chaing pozision"
 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>$LOG_FILE_NAM
 VALIDATE $? "unzipping"
 
-cp home/ec2-user/expense-shell/expense.conf  /etc/nginx/default.d/expense.conf
+cp home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOG_FILE_NAM
 VALIDATE $? "editing congig"
 
