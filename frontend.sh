4b56c47b660e7b956d00e269d6f04cc8e08d8810
prem36999 <premdevops369@gmail.com>
2026-07-10 19:59:20 +0530

    expense shell added

---
+++ frontend.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOG_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){ 
+    if [ $1 -ne 0 ]
+  then
+     echo -e "$2 ... $R FAILURE $N "
+     exit 1
+  else
+     echo -e "$2...$G success $N"
+  fi
+}
+
+CHECK_ROOT(){
+  if [ $USERID -ne 0 ]
+  then
+    echo "ERROR:: you must have sudo access to execute the script"  
+    exit 1 #other than 0
+  fi
+
+}   
+ 
+echo "script started excuting at $TIMESTAMP " &>>$LOG_FILE_NAME 
+
+CHECK_ROOT
+
+dnf install nginx -y 
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
