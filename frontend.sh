0b3add2f42c80ae3c4e158c1374a7d69a4f3f34a
prem36999 <premdevops369@gmail.com>
2026-07-10 20:09:34 +0530

    expense shell added

--- frontend.sh
+++ frontend.sh
@@ -38,3 +38,6 @@ systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+cp home/ec2-user/ /etc/nginx/default.d/expense.conf
